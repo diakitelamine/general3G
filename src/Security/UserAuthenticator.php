6ed0340c1af4623e52d29054d57b5bbf9128a773
diakitelamine <42340818+diakitelamine@users.noreply.github.com>
2021-04-09 14:04:47 +0200

    Ajustements

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -95,8 +95,7 @@ class UserAuthenticator extends AbstractFormLoginAuthenticator implements Passwo
         if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
             return new RedirectResponse($targetPath);
         }
-
-        // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
+        return new RedirectResponse($this->urlGenerator->generate('home'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
